571df73ceae0428f9b09abf0a1dfc7be7deede25
Ditto <myriadarrows0@gmail.com>
2024-12-15 12:27:10 +0000

    Create ExampleBatch.cmd

---
+++ Tools/CSVDefCreator/ExampleBatch.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+@rem Make paths relative to the main buildfile folder
+cd ..\..\
+
+@rem Call the executable
+@rem Usage: CSVDefCreator.exe PathToDefinitionsFolder PathToTable PathToTable PathToTable...
+Tools\CSVDefCreator\CSVDefCreator.exe Definitions\ Tables\NightmareModules\CharactersClasses\CharacterTable.csv
+
+@rem pause because executable doesn't automatically pause from expected execution; means it can be used in MAKE_HACK processes.
+pause
